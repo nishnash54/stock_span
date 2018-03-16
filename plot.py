b90fa627ff89a2504cd41def7a4e3aa73d7402f3
Nishant Rodrigues <nishnash54@gmail.com>
2018-03-16 19:48:30 +0530

    Bar graph

--- plot.py
+++ plot.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as plt
 from dateutil.parser import parse
 
-def plot(x, y):
+def plot(x, y, S):
 
     x = [parse(i) for i in x]
-    x_plot = [i for i in range(1, len(x)+1)]
+    x_plot = [i for i in range(1, 2*len(x), 2)]
     print x, y
 
-    plt.plot(x_plot, y, color='red', alpha=0.75)
-    plt.title('Stock prices')
+    x_plot = x_plot[:15]
+    y = y[:15]
+
+    plt.bar(x_plot, y, color='red', alpha=0.75)
+    val = 0
+    for i, j in zip(x_plot, y):
+        plt.annotate(str(S[val]), xy=(i, j+0.25))
+        val += 1
+    plt.title('Stock span problem')
     plt.show()
