8df6d50d3b9f6bab7e7fb0827118147d42bbd1c9
Nishant Rodrigues <nishnash54@gmail.com>
2018-03-16 19:44:20 +0530

    Calculate span

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from data import return_data
 from plot import plot
 
+def calculateSpan(price, n, S):
+
+    S[0] = 1
+    for i in range(1, n, 1):
+        S[i] = 1
+        j = i - 1
+        while (j>=0) and (price[i] >= price[j]) :
+            S[i] += 1
+            j -= 1
+
 if __name__ == "__main__":
 
     sym = "MSFT"
-
     x_data, y_data = return_data(sym)
-    plot(x_data, y_data)
+    num = len(y_data)
+    S = [None] * num
+    calculateSpan(y_data, num, S)
+    print S
+    plot(x_data, y_data, S)
